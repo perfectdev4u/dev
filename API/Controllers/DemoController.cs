5ef630982d9d1214dbd5d4db088cfd8d53b82873
beyond-root-dev <perfectdev4u@gmail.com>
2023-02-13 12:27:20 +0530

    Initial commit

---
+++ API/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+       public string DemoAPI()
+        {
+            return null;
+        }
+    }
+}
